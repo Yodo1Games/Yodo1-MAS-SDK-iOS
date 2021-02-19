b5680c76f84c5a374dfa3387a646b01e26044b8e
ZhouYuzhen <940549652@qq.com>
2021-02-19 16:44:52 +0800

    Yandex Mediation, IronSource Mediation,AdMob Mediation

---
+++ Yodo1MasMediationFacebook/Lib/FacebookYandexMobileAdsAdapters.framework/Versions/A/Headers/YMAFacebookVersion.h
@@ -0,0 +1,15 @@
+/*
+ * Version for iOS © 2015–2021 YANDEX
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://yandex.com/legal/mobileads_sdk_agreement/
+ */
+
+#ifndef YMA_FACEBOOK_VERSION
+#define YMA_FACEBOOK_VERSION
+
+#define YMA_FACEBOOK_VERSION_MAJOR 0
+#define YMA_FACEBOOK_VERSION_MINOR 18
+#define YMA_FACEBOOK_VERSION_PATCH 0
+
+#endif // YMA_FACEBOOK_VERSION
