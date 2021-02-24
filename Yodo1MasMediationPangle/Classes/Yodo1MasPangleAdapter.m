cfbc3aafa78907ad039a9336c2debe1005f15a6e
joyhubs <joyhubs@163.com>
2021-02-24 09:50:11 +0800

    今日头条广告平台

--- Yodo1MasMediationPangle/Classes/Yodo1MasPangleAdapter.m
+++ Yodo1MasMediationPangle/Classes/Yodo1MasPangleAdapter.m
@@ -6,22 +6,32 @@
 //
 
 #import "Yodo1MasPangleAdapter.h"
+#import <BUAdSDK/BUAdSDK.h>
 
 #define BANNER_TAG 10011
 
-@interface Yodo1MasPangleAdapter()
+@interface Yodo1MasPangleAdapter()<BURewardedVideoAdDelegate> {
+    BOOL isRewardAdReady;
+    BOOL isInterstitialAdReady;
+}
 
+
+@property(nonatomic, assign) BOOL sdkInit;
+//@property (nonatomic, strong) MTRGAdView *bannerAd;
+//@property (nonatomic, strong) MTRGInterstitialAd *interstitialAd;
+@property (nonatomic, strong) BURewardedVideoAd *rewardAd;
+@property (nonatomic,strong) BURewardedVideoModel *rewardModel;
 
 @end
 
 @implementation Yodo1MasPangleAdapter
 
 - (NSString *)advertCode {
-    return @"adcolony";
+    return @"pangle";
 }
 
 - (NSString *)sdkVersion {
-    return @"1.0.0";
+    return BUAdSDKManager.SDKVersion;
 }
 
 - (NSString *)mediationVersion {
@@ -32,7 +42,10 @@
     [super initWithConfig:config successful:successful fail:fail];
     
     if (![self isInitSDK]) {
-        
+        [self updatePrivacy];
+        [BUAdSDKManager setAppID:config.appId];
+        [BUAdSDKManager setIsPaidApp:NO];
+        self.sdkInit = YES;
     } else {
         if (successful != nil) {
             successful(self.advertCode);
@@ -41,24 +54,27 @@
 }
 
 - (BOOL)isInitSDK {
-    return [super isInitSDK];
+    [super isInitSDK];
+    return self.sdkInit;
 }
 
 - (void)updatePrivacy {
     [super updatePrivacy];
+    [BUAdSDKManager setGDPR:Yodo1Mas.sharedInstance.isGDPRUserConsent];
+    [BUAdSDKManager setCoppa:Yodo1Mas.sharedInstance.isCOPPAAgeRestricted];
 }
 
 
 #pragma mark - 激励广告
 - (BOOL)isRewardAdLoaded {
-    return [super isRewardAdLoaded];
+    [super isRewardAdLoaded];
+    return isRewardAdReady;
 }
 
 - (void)loadRewardAd {
     [super loadRewardAd];
     if (![self isInitSDK]) return;
-    
-    Yodo1MasAdId *adId = [self getRewardAdId];
+    [self.rewardAd loadAdData];
 }
 
 - (void)showRewardAd:(Yodo1MasAdCallback)callback object:(NSDictionary *)object {
@@ -67,12 +83,25 @@
         UIViewController *controller = [Yodo1MasPangleAdapter getTopViewController];
         if (controller != nil) {
             NSLog(@"%@:{method: showRewardAd, show reward ad...}", self.TAG);
+            [self.rewardAd showAdFromRootViewController:controller];
         }
     }
 }
 
 - (void)dismissRewardAd {
     
+}
+
+- (BURewardedVideoAd *)rewardAd {
+    if (!_rewardAd) {
+       _rewardModel = [[BURewardedVideoModel alloc] init];
+        _rewardModel.userId = @"";
+        Yodo1MasAdId *adId = [self getRewardAdId];
+        _rewardAd = [[BURewardedVideoAd alloc] initWithSlotID:adId.adId
+                                           rewardedVideoModel:_rewardModel];
+        _rewardAd.delegate = self;
+    }
+    return _rewardAd;
 }
 
 #pragma mark - 插屏广告
@@ -83,8 +112,6 @@
 - (void)loadInterstitialAd {
     [super loadInterstitialAd];
     if (![self isInitSDK]) return;
-    
-    Yodo1MasAdId *adId = [self getInterstitialAdId];
 }
 
 - (void)showInterstitialAd:(Yodo1MasAdCallback)callback object:(NSDictionary *)object {
@@ -138,5 +165,57 @@
         [self loadBannerAd];
     }
 }
+
+
+#pragma mark BURewardedVideoAdDelegate
+
+- (void)rewardedVideoAdVideoDidLoad:(BURewardedVideoAd *)rewardedVideoAd {
+    isRewardAdReady = YES;
+}
+
+
+- (void)rewardedVideoAd:(BURewardedVideoAd *)rewardedVideoAd didFailWithError:(NSError *)error {
+    isRewardAdReady = NO;
+    NSString *message = [NSString stringWithFormat:@"%@: {method: didFailWithError:, error: %@}", self.TAG, error.localizedDescription];
+    NSLog(@"%@", message);
+    Yodo1MasError *rewardError = [[Yodo1MasError alloc] initWitCode:Yodo1MasErrorCodeAdLoadFail message:message];
+    [self callbackWithError:rewardError type:Yodo1MasAdTypeReward];
+    [self nextReward];
+    [self loadRewardAd];
+}
+
+- (void)rewardedVideoAdWillVisible:(BURewardedVideoAd *)rewardedVideoAd {
+    [self callbackWithEvent:Yodo1MasAdEventCodeOpened type:Yodo1MasAdTypeReward];
+}
+
+- (void)rewardedVideoAdDidClose:(BURewardedVideoAd *)rewardedVideoAd {
+    self.rewardAd = nil;
+    self.rewardModel = nil;
+    [self callbackWithEvent:Yodo1MasAdEventCodeClosed type:Yodo1MasAdTypeReward];
+    [self nextReward];
+    [self loadRewardAd];
+}
+
+
+/// 激励视频广告播放完成或发生错误
+- (void)rewardedVideoAdDidPlayFinish:(BURewardedVideoAd *)rewardedVideoAd didFailWithError:(NSError *)error {
+     
+}
+
+- (void)rewardedVideoAdServerRewardDidFail:(BURewardedVideoAd *)rewardedVideoAd error:(NSError *)error {
+   
+}
+
+- (void)rewardedVideoAdServerRewardDidSucceed:(BURewardedVideoAd *)rewardedVideoAd verify:(BOOL)verify{
+    if (verify) {
+        [self callbackWithEvent:Yodo1MasAdEventCodeRewardEarned type:Yodo1MasAdTypeReward];
+    }
+}
+
+/// 激励视频广告点击
+- (void)rewardedVideoAdDidClick:(BURewardedVideoAd *)rewardedVideoAd {
+   
+}
+
 
 @end
