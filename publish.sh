e30b2ba0d217ea64d2b2801f95a0f26f11d68f56
ZhouYuzhen <940549652@qq.com>
2021-05-11 18:41:11 +0800

    ci

--- publish.sh
+++ publish.sh
@@ -60,72 +60,72 @@ do
     done < ${podfile}
 done
 
-# 上传Cocoapods
-# cocoapodsSpecs="https://github.com/CocoaPods/Specs.git,https://github.com/Yodo1Games/Yodo1Spec.git"
-# repositoryName="Yodo1Mas-${env}"
-# repositoryToken=""
-# privateSpecs=""
-# if [[ ${token} != '' ]]
-# then
-#     repositoryToken="x-access-token:${token}@"
-# fi
+上传Cocoapods
+cocoapodsSpecs="https://github.com/CocoaPods/Specs.git,https://github.com/Yodo1Games/Yodo1Spec.git"
+repositoryName="Yodo1Mas-${env}"
+repositoryToken=""
+privateSpecs=""
+if [[ ${token} != '' ]]
+then
+    repositoryToken="x-access-token:${token}@"
+fi
 
-# if [[ ${env} == "Release" ]]
-# then
-#     privateSpecs="https://${repositoryToken}github.com/Yodo1Games/MAS-Spec.git"
-# else
-#     privateSpecs="https://${repositoryToken}github.com/Yodo1Games/MAS-Spec-${env}.git"
-# fi
+if [[ ${env} == "Release" ]]
+then
+    privateSpecs="https://${repositoryToken}github.com/Yodo1Games/MAS-Spec.git"
+else
+    privateSpecs="https://${repositoryToken}github.com/Yodo1Games/MAS-Spec-${env}.git"
+fi
 
-# echo 仓库地址:${privateSpecs}
+echo 仓库地址:${privateSpecs}
 
-# pod repo add $repositoryName $privateSpecs
-# pod repo push $repositoryName build/Yodo1MasCore.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs}"
+pod repo add $repositoryName $privateSpecs
+pod repo push $repositoryName build/Yodo1MasCore.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs}"
 
-# # 先将非Max的Mediation上传至Cocoapods
-# for podfile in $(find . -maxdepth 1 -name "Yodo1MasMediation*.podspec" | sort)
-# do
-#     if [[ ! ${podfile} =~ "Max.podspec" ]]
-#     then
-#         name=${podfile:2}
-#         echo 上传Cocoapods:${name}
-#         pod repo push $repositoryName build/$name --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
-#     fi
-# done
+# 先将非Max的Mediation上传至Cocoapods
+for podfile in $(find . -maxdepth 1 -name "Yodo1MasMediation*.podspec" | sort)
+do
+    if [[ ! ${podfile} =~ "Max.podspec" ]]
+    then
+        name=${podfile:2}
+        echo 上传Cocoapods:${name}
+        pod repo push $repositoryName build/$name --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
+    fi
+done
 
-# # 将Max的Mediation上传至Cocoapods
-# for podfile in $(find . -maxdepth 1 -name "Yodo1MasMediation*.podspec" | sort)
-# do
-#     if [[ ${podfile} =~ "Max.podspec" ]]
-#     then
-#         name=${podfile:2}
-#         echo 上传Cocoapods:${name}
-#         pod repo push $repositoryName build/$name --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
-#     fi
-# done
+# 将Max的Mediation上传至Cocoapods
+for podfile in $(find . -maxdepth 1 -name "Yodo1MasMediation*.podspec" | sort)
+do
+    if [[ ${podfile} =~ "Max.podspec" ]]
+    then
+        name=${podfile:2}
+        echo 上传Cocoapods:${name}
+        pod repo push $repositoryName build/$name --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
+    fi
+done
 
-# pod repo push $repositoryName build/Yodo1MasCN.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
-# pod repo push $repositoryName build/Yodo1MasStandard.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
-# pod repo push $repositoryName build/Yodo1MasFull.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
+pod repo push $repositoryName build/Yodo1MasCN.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
+pod repo push $repositoryName build/Yodo1MasStandard.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
+pod repo push $repositoryName build/Yodo1MasFull.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
 
-# #  修改Yodo1MasMediationFacebook
-# name='Yodo1MasMediationFacebook'
-# version=''
-# while read line
-# do
-#     if [[ ${line} == s.version* ]]
-#     then
-#         version="$(echo ${line} | tr -d '[:space:]' | tr -d \')"
-#         version="${version:10}"
-#         break
-#     fi
-# done < build/${name}.podspec
+#  修改Yodo1MasMediationFacebook
+name='Yodo1MasMediationFacebook'
+version=''
+while read line
+do
+    if [[ ${line} == s.version* ]]
+    then
+        version="$(echo ${line} | tr -d '[:space:]' | tr -d \')"
+        version="${version:10}"
+        break
+    fi
+done < build/${name}.podspec
 
-# cd ~/.cocoapods/repos/${repositoryName}/${name}/${version}
-# sed -i "" '39c\'$'\n  # s.dependency \'FBAudienceNetwork\', \'6.2.1\'\n' ${name}.podspec
-# sed -i "" '39a\'$'\n  s.vendored_frameworks = s.name + \'/Lib/**/*.framework\'\n' ${name}.podspec
-# git add ${name}.podspec
-# git commit -m "[Update] ${name} (${version})"
-# git push origin main
+cd ~/.cocoapods/repos/${repositoryName}/${name}/${version}
+sed -i "" '39c\'$'\n  # s.dependency \'FBAudienceNetwork\', \'6.2.1\'\n' ${name}.podspec
+sed -i "" '39a\'$'\n  s.vendored_frameworks = s.name + \'/Lib/**/*.framework\'\n' ${name}.podspec
+git add ${name}.podspec
+git commit -m "[Update] ${name} (${version})"
+git push origin main
 
 echo 上传Cocoapods结束:$(date +%Y-%m-%d\ %H:%M:%S) 
