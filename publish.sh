01cfa4a9002c982e29aeb36b7ce0780b815789eb
ZhouYuzhen <940549652@qq.com>
2021-05-12 16:53:23 +0800

    CI优化
    - 加入缓存机制
    - CI执行完成发送钉钉机器人消息

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@
 envs=(Dev Pre Release)
 env=$1
 token=$2
+dingtalkToken=$3
 
 if [[ ${env} == '' ]]
 then
@@ -37,15 +38,20 @@ do
         fi
     done < ${podfile}
 
+    logFile=build/log/${name}.txt
     # 压缩源码
     filename="${name}-${version}.zip"
     echo "压缩${name} -> ${filename} ...."
-    zip -r build/zip/${filename} ${name} -r
+    echo "压缩文件============================" >> ${logFile}
+    zip -r build/zip/${filename} ${name} -r >> ${logFile}
+    echo "\n\n" >> ${logFile}
 
     # 上传地址
     url="https://mas-artifacts.yodo1.com/${version}/iOS/${filename}"
     echo "上传:${url}"
-    ./ossutilmac64 cp build/zip/${filename} oss://yodo1-mas-sdk/${version}/iOS/ -c ~/.ossutilconfig -u
+    echo "上传文件============================" >> ${logFile}
+    ./ossutilmac64 cp build/zip/${filename} oss://yodo1-mas-sdk/${version}/iOS/ -c ~/.ossutilconfig -u >> ${logFile}
+    echo "\n\n" >> ${logFile}
 
     # 修改podspec文件的s.sources
     echo "" > build/${name}.podspec
@@ -80,8 +86,9 @@ fi
 
 echo 仓库地址:${privateSpecs}
 
+echo "上传Cocoapods============================" >> build/log/Yodo1MasCore.txt
 pod repo add $repositoryName $privateSpecs
-pod repo push $repositoryName build/Yodo1MasCore.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs}"
+pod repo push $repositoryName build/Yodo1MasCore.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs}" >> build/log/Yodo1MasCore.txt
 
 # 先将非Max的Mediation上传至Cocoapods
 for podfile in $(find . -maxdepth 1 -name "Yodo1MasMediation*.podspec" | sort)
@@ -89,8 +96,10 @@ do
     if [[ ! ${podfile} =~ "Max.podspec" ]]
     then
         name=${podfile:2}
+        logFile=build/log/${name/".podspec"}.txt
         echo 上传Cocoapods:${name}
-        pod repo push $repositoryName build/$name --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
+        echo "上传Cocoapods============================" >> ${logFile}
+        pod repo push $repositoryName build/$name --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}" >> ${logFile}
     fi
 done
 
@@ -100,14 +109,65 @@ do
     if [[ ${podfile} =~ "Max.podspec" ]]
     then
         name=${podfile:2}
+        logFile=build/log/${name/".podspec"}.txt
         echo 上传Cocoapods:${name}
-        pod repo push $repositoryName build/$name --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
+        echo "上传Cocoapods============================" >> ${logFile}
+        pod repo push $repositoryName build/$name --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}" >> ${logFile}
     fi
 done
 
-pod repo push $repositoryName build/Yodo1MasCN.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
-pod repo push $repositoryName build/Yodo1MasStandard.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
-pod repo push $repositoryName build/Yodo1MasFull.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}"
+echo "上传Cocoapods============================" >> build/log/Yodo1MasCN.txt
+pod repo push $repositoryName build/Yodo1MasCN.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}" >> build/log/Yodo1MasCN.txt
+
+echo "上传Cocoapods============================" >> build/log/Yodo1MasStandard.txt
+pod repo push $repositoryName build/Yodo1MasStandard.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}" >> build/log/Yodo1MasStandard.txt
+
+echo "上传Cocoapods============================" >> build/log/Yodo1MasFull.txt
+pod repo push $repositoryName build/Yodo1MasFull.podspec --verbose --use-libraries --allow-warnings --sources="${cocoapodsSpecs},${privateSpecs}" >> build/log/Yodo1MasStandard.txt
+
+# 发送钉钉机器人消息
+if [[ ${dingtalkToken} == '' ]]
+then
+    echo "Token为空，无法发送钉钉机器人消息"
+else
+    msgTitle="Yodo1Mas iOS发布完成"
+    msgContent=""
+    for podfile in $(find . -maxdepth 1 -name "*.podspec" | sort)
+    do
+        # 获取文件名和版本号
+        name="${podfile:2}"
+        name="${name/".podspec"}"
+        version=""
+
+        if [[ ${name} == 'Yodo1MasUnityBridge' ]]
+        then
+            continue
+        fi
+
+        while read line
+        do
+            if [[ ${line} == s.version* ]]
+            then
+                version="$(echo ${line} | tr -d '[:space:]' | tr -d \')"
+                version="${version:10}"
+                break
+            fi
+        done < ${podfile}
+
+        logFile=build/log/${name}.txt
+        if [ ! -d ~/.cocoapods/repos/${repositoryName}/${name}/${version}/ ]
+        then
+            ./ossutilmac64 cp ${logFile} oss://yodo1-mas-sdk/${version}/iOS/ -c ~/.ossutilconfig -u
+            msgContent="${msgContent}\n- ${name} 失败: 请查看[日志](https://mas-artifacts.yodo1.com/${version}/iOS/${name}.txt)"
+        else
+            msgContent="${msgContent}\n- ${name} 成功"
+        fi
+    done
+    
+    curl "https://oapi.dingtalk.com/robot/send?access_token=${dingtalkToken}" \                    
+        -H "Content-Type: application/json" \                             
+        -d "{\"msgtype\": \"markdown\",\"markdown\": {\"title\":\"Actions:${msgTitle}\",\"text\":\"${msgContent}\",\"at\":{\"isAtAll\":true}}}"
+fi
 
 #  修改Yodo1MasMediationFacebook
 echo "修改Yodo1MasMediationFacebook.podspec ..."
@@ -122,13 +182,11 @@ do
         break
     fi
 done < build/${name}.podspec
-
 cd ~/.cocoapods/repos/${repositoryName}/
 sed -i "" '39c\'$'\n  # s.dependency \'FBAudienceNetwork\', \'6.2.1\'\n' ${name}/${version}/${name}.podspec
 sed -i "" '39a\'$'\n  s.vendored_frameworks = s.name + \'/Lib/**/*.framework\'\n' ${name}/${version}/${name}.podspec
 git add .
 git commit -m "[Fix] ${name} (${version})"
-git remote add origin ${privateSpecs}
 git push -u origin main
 
 echo 上传Cocoapods结束:$(date +%Y-%m-%d\ %H:%M:%S) 
