1c772d48fb32dbb928d04ccd5cca4fd74349ef40
ZhouYuzhen <940549652@qq.com>
2021-07-09 18:01:16 +0800

    CI优化

--- Yodo1MasSdk/publish.sh
+++ Yodo1MasSdk/publish.sh
@@ -1,33 +1,43 @@
 #!/bin/sh -e
 
-envs=(Dev Pre Release)
-env=$1
+branch=$1
 token=$2
 dingtalkToken=$3
 
-if [[ ${env} == '' ]]
-then
-    env=${envs[0]}
-    echo "环境参数为空，默认设置为${envs[0]}"
-fi
+# 读取配置文件
+version=""
+suffix=""
+env=""
+while read line
+do
+    if [[ ${line} == *version* ]]
+    then
+        version="${line:8}"
+    elif [[ ${line} == *suffix* ]]
+    then
+        suffix="${line:7}"
+    elif [[ ${line} == *env* ]]
+    then
+        env="${line:4}"
+    fi
+done < config.txt
 
-if [[ ! "${envs[@]}" =~ "$env" ]]
+if [[ ${branch} == *master* ]]
 then
-    env=${envs[0]}
-    echo "环境参数值不为${envs[@]}，默认设置为${envs[0]}"
+    env='Release'
+else
+    if [[ ${env} == 'Release' ]]
+    then
+        env='dev'
+    fi
+    version="${version}-${suffix}"
 fi
 
 echo 上传Cocoapods开始:$(date +%Y-%m-%d\ %H:%M:%S) 
 
+# 上传Cocoapods
 cd Yodo1MasSdk
 sh prepare.sh
-
-version=""
-while read line
-do
-    version=${line}
-done < version.txt
-echo ${version}
 
 sh publish-one.sh ${env} Yodo1MasCore ${version} '' ${token}
 
@@ -48,40 +58,32 @@ sh publish-one.sh ${env} Yodo1MasStandard ${version} '' ${token}
 sh publish-one.sh ${env} Yodo1MasFull ${version} '' ${token}
 
 repositoryName="Yodo1Mas-${env}"
-# 发送钉钉机器人消息
-if [[ ${dingtalkToken} == '' ]]
-then
-    echo "Token为空，无法发送钉钉机器人消息"
-else
-    msgTitle="Actions:Release iOS Yodo1MasSDK"
-    msgContent="#### ${msgTitle}"
-    msgContent="${msgContent}\n Result: Actions Completed"
-    msgContent="${msgContent}\n Environment: ${env}"
-    msgContent="${msgContent}\n Version: ${version}"
-    msgContent="${msgContent}\n ##### Detail"
-    for podfile in $(find build -maxdepth 1 -name "*.podspec" | sort)
-    do
-        # 获取文件名和版本号
-        name="${podfile:6}"
-        name="${name/".podspec"}"
+# 验证上传Cocoapods是否成功
+successful=true
+msgTitle="Actions:Release iOS Yodo1MasSDK"
+msgContent="#### ${msgTitle}\nResult: Actions Completed\nEnvironment: ${env}\nVersion: ${version}\n##### Detail"
+for podfile in $(find build -maxdepth 1 -name "*.podspec" | sort)
+do
+    # 获取文件名和版本号
+    name="${podfile:6}"
+    name="${name/".podspec"}"
 
-        if [[ ${name} == 'Yodo1MasUnityBridge' ]]
-        then
-            continue
-        fi
+    if [[ ${name} == 'Yodo1MasUnityBridge' ]]
+    then
+        continue
+    fi
 
-        logFile=build/log/${name}.txt
-        if [ ! -d ~/.cocoapods/repos/${repositoryName}/${name}/${version}/ ]
-        then
-            ./ossutilmac64 cp ${logFile} oss://yodo1-mas-sdk/${version}/iOS/ -c ~/.ossutilconfig -u
-            msgContent="${msgContent}\n- ${name} 失败: 请查看[日志](https://mas-artifacts.yodo1.com/${version}/iOS/${name}.txt)"
-        else
-            msgContent="${msgContent}\n- ${name} 成功"
-        fi
-    done
-    
-    curl "https://oapi.dingtalk.com/robot/send?access_token=${dingtalkToken}" -H "Content-Type: application/json" -d "{\"msgtype\": \"markdown\",\"markdown\": {\"title\":\"Actions:${msgTitle}\",\"text\":\"${msgContent}\",\"at\":{\"isAtAll\":true}}}"
-fi
+    logFile=build/log/${name}.txt
+    if [ ! -d ~/.cocoapods/repos/${repositoryName}/${name}/${version}/ ]
+    then
+        ./ossutilmac64 cp ${logFile} oss://yodo1-mas-sdk/${version}/iOS/ -c ~/.ossutilconfig -u
+        msgContent="${msgContent}\n- ${name} 失败: 请查看[日志](https://mas-artifacts.yodo1.com/${version}/iOS/${name}.txt)"
+        successful=false
+    else
+        msgContent="${msgContent}\n- ${name} 成功"
+    fi
+done
+echo "{\"successful\" : \"${successful}\", \"version\" : \"${version}\" }" > Yodo1Mas.json
 
 #  修改Yodo1MasMediationFacebook
 echo "修改Yodo1MasMediationFacebook.podspec ..."
@@ -112,3 +114,19 @@ git commit -m "[Fix] ${name} (${version})"
 git push -u origin ${originName}
 
 echo 上传Cocoapods结束:$(date +%Y-%m-%d\ %H:%M:%S) 
+
+
+# 发送钉钉机器人消息
+if [[ ${dingtalkToken} == '' ]]
+then
+    echo "Token为空，无法发送钉钉机器人消息"
+else
+    curl "https://oapi.dingtalk.com/robot/send?access_token=${dingtalkToken}" -H "Content-Type: application/json" -d "{\"msgtype\": \"markdown\",\"markdown\": {\"title\":\"Actions:${msgTitle}\",\"text\":\"${msgContent}\",\"at\":{\"isAtAll\":true}}}"
+fi
+
+if [[ ${successful} == 1 ]]
+then
+    exit 0
+else
+    exit 1
+fi
